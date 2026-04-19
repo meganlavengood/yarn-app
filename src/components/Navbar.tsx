0e104b68a391bec9bc36fcee0943be35be28d4c5
Megan <60526470+meganlavengood@users.noreply.github.com>
2026-04-19 16:12:25 -0400

    replace svg with png

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,7 +16,7 @@ function Navbar({ user }: NavbarProps) {
 			<nav className="navbar flex items-end justify-between">
 				<Link to="/">
 					<div className="nav-brand flex items-end flex-nowrap">
-						<img src="/src/assets/ball.svg" className="size-1/8" />
+						<img src="/src/assets/ball.png" className="size-1/8" />
 						<h1 className="text-2xl md:text-5xl">The Yarn Store</h1>
 					</div>
 				</Link>
